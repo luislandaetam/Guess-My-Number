eac6d1265274302a8b362dc04d6cf2ad4c93dd00
Luis Landaeta <64661923+luislandaetam@users.noreply.github.com>
2023-02-02 10:53:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let score = 20;
+let highscore = 0;
+
+let check = document.getElementById("check");
+let again = document.getElementById("again");
+
+function displayMessage(message) {
+  document.getElementById("message").textContent = message;
+}
+
+function guess() {
+  let number = Number(document.getElementById("number").value);
+  if (score === 0) {
+    return displayMessage("🕹️ Please, reset the game!");
+  }
+
+  if (number > 20 || number < 1) {
+    return displayMessage("💡 The number must be between 1 and 20");
+  }
+
+  if (number === secretNumber) {
+    document.getElementById("secret_number").textContent = secretNumber;
+    document.getElementById("secret_number").style.padding = "1rem 8rem";
+    displayMessage("🥳 You nailed it!");
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.getElementById("number").style.backgroundColor = "#60b347";
+
+    if (score > highscore) {
+      document.getElementById("highscore").textContent = score;
+    }
+  } else {
+    score--;
+    document.getElementById("score").textContent = score;
+    document.getElementById("number").value = "";
+
+    if (score < 1) {
+      return displayMessage("😭 GAME OVER!");
+    }
+
+    if (number > secretNumber) {
+      displayMessage("📈 Too High!");
+    } else {
+      displayMessage("📉 Too Low!");
+    }
+  }
+}
+
+function repeat() {
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector("#number").style.backgroundColor = "#222";
+  document.querySelector("#number").value = "";
+  document.querySelector("#secret_number").textContent = "?";
+  document.querySelector("#secret_number").style.padding = "1rem 4rem";
+  document.querySelector("#score").textContent = "20";
+  document.querySelector("#message").textContent = "Start guessing...";
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+}
+
+check.addEventListener("click", guess);
+again.addEventListener("click", repeat);
